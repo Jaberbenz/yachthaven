b145ba4d856eaab82122cf37d857cd0e96be2fd0
jaber <jaberksh3012@gmail.com>
2024-02-20 16:43:48 +0100

    faire l'athentification de la bdd

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import {Auth} from "./components/auth"
+
+function App() {
+
+  return (
+    
+      <div className="App">
+       <Auth />
+       
+      </div>
+
+  );
+}
+
+export default App
---
+++ src/config/firebase.jsx
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0wc4Wd35KfjHvEH69MJkUdkhlFMRM-Zg",
+  authDomain: "yachthaven-f588e.firebaseapp.com",
+  projectId: "yachthaven-f588e",
+  storageBucket: "yachthaven-f588e.appspot.com",
+  messagingSenderId: "516684670659",
+  appId: "1:516684670659:web:c30ef7359316c652056476",
+  measurementId: "G-XNH498VHDL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth }
